f861cf90476ea174128ebab4b51aca4cf4f5b8f2
LPaifar <lucaspaifar1996@gmail.com>
2021-02-19 12:04:53 -0300

    Projeto Spring

---
+++ SpringBoot PROJETO INTEGRADOR/EducAcao/src/main/java/ong/generation/EducAcao/EducAcaoApplication.java
@@ -0,0 +1,13 @@
+package ong.generation.EducAcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducAcaoApplication.class, args);
+	}
+
+}
