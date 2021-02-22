5d9a1e9de4942e46714e36fcbc7bea40b0407ca0
Esteban <gustavoesteban13@hotmail.com>
2021-02-22 10:16:29 -0300

    Adicionando properties

--- SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/educAcaoApplication.java
+++ SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/educAcaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EducAcaoApplication {
+public class educAcaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EducAcaoApplication.class, args);
+		SpringApplication.run(educAcaoApplication.class, args);
 	}
 
 }
