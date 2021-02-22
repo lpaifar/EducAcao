303ba7fbccdfbcffbf73358564037be2bbd081fc
Igor M <iggorm@yahoo.com.br>
2021-02-22 18:30:09 -0300

    Inicialização Spring

--- SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/repository/UsuarioRepository.java
+++ SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/repository/UsuarioRepository.java
@@ -8,10 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
 	
-<<<<<<< HEAD
-	public List<Usuario> findAllByNomeCompletoContainingIgnoreCase( String nome_Completo);
-=======
 	public List<Usuario> findAllByNomeCompletoContainingIgnoreCase( String nomeCompleto);
->>>>>>> 1a9ede4aededa6ebc69f1cc7c84eab44cc88fb87
 }
 
