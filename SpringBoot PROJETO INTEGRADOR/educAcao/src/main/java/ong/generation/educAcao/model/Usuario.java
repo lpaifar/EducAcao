f397312db90b01ed35c349cef1c0aba86fe59641
Igor M <iggorm@yahoo.com.br>
2021-02-22 15:33:24 -0300

    inserindo model usuario

--- SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/model/Usuario.java
+++ SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/model/Usuario.java
@@ -1,5 +1,79 @@
 package ong.generation.educAcao.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import ong.generation.EducAcao.model.Postagem;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome_Completo;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome_Completo() {
+		return nome_Completo;
+	}
+
+	public void setNome_Completo(String nome_Completo) {
+		this.nome_Completo = nome_Completo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}	
 
 }
