f420d5249a672190212e5221fb219ec6399ce487
Igor M <iggorm@yahoo.com.br>
2021-02-22 16:57:32 -0300

    adicionando Usuario Repository

--- SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/repository/UsuarioRepository.java
+++ SpringBoot PROJETO INTEGRADOR/educAcao/src/main/java/ong/generation/educAcao/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package ong.generation.educAcao.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
 
+import ong.generation.educAcao.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository <Postagem, Long>{
+	public List<Postagem> findAllByNome_CompletoContainingIgnoreCase( String nome_Completo);
 }
